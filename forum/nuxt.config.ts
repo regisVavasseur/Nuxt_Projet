a8017dac4fc44c4b0a4627c96cc9751f94974e4b
Antoine Stoecklin <astoeck@hotmail.fr>
2024-03-27 14:41:31 +0100

    nuxt config for vuetify

--- forum/nuxt.config.ts
+++ forum/nuxt.config.ts
@@ -1,4 +1,26 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
