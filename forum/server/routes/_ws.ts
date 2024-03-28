ad17a0883de30f345e42e4a885d0d80105152b4c
Antoine Stoecklin <astoeck@hotmail.fr>
2024-03-28 17:24:11 +0100

    web socket done correctly

--- forum/server/routes/_ws.ts
+++ forum/server/routes/_ws.ts
@@ -1,25 +1,27 @@
 
+import WebSocket from "ws";
+
 export default defineWebSocketHandler({
   async handle(request, ws: WebSocket) {
     console.log('Client connected');
     const peers = new Set<WebSocket>();
     peers.add(ws);
 
-    ws.on('message', (message: string) => {
-      console.log(`Message received: ${message}`);
+    ws.addEventListener('message', (event) => {
+      console.log(`Message received: ${event.data}`);
 
       // Broadcast the message to all connected clients
       peers.forEach((client) => {
-        client.send(message);
+        client.send(event.data);
       });
     });
 
-    ws.on('close', () => {
+    ws.addEventListener('close', () => {
       console.log('Client disconnected');
       peers.delete(ws);
     });
 
-    ws.on('error', (error) => {
+    ws.addEventListener('error', (error) => {
       console.log(`[ws] error: ${error.message}`);
     });
   }
