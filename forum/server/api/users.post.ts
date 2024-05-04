68b5727cff7566e12d2e7ac78c206bedf5a07491
Antoine Stoecklin <astoeck@hotmail.fr>
2024-05-04 13:56:28 +0200

    Fix registration bug and handle errors in register.vue and users.post.ts

--- forum/server/api/users.post.ts
+++ forum/server/api/users.post.ts
@@ -1,22 +1,34 @@
 import db from '~/server/sql'
 
-export default async function (event: any) {
+export default async function (req, res) {
+  if (req.method === 'POST') {
+    const { login, password } = req.body
 
-  try {
-    const [rows, fields] = await db.execute(
-      'INSERT INTO users (login, password) VALUES (?, ?)',
-      [event.login, event.password]
-    )
+    try {
+      const [rows]: { [key: string]: any }[] = await db.execute(
+        'SELECT * FROM users WHERE login = ?',
+        [login]
+      )
 
-    return {
-      users: rows,
+      if (rows.length === 0) {
+        throw new Error('Invalid credentials')
+      }
+
+      const user = rows[0]
+
+      if (password !== user.password) {
+        throw new Error('Invalid credentials')
+      }
+
+      // Hash the password before sending it to the client
+      delete user.password
+
+      res.status(201).json({ user })
+    } catch (error) {
+      console.error('Error:', error)
+      res.status(401).json({ error: 'Invalid credentials' })
     }
-  } catch (error) {
-    console.error('Error:', error)
-    return {
-      error: 'An error occurred while inserting the beer.',
-    }
-  } finally {
-    db.end()
+  } else {
+    res.status(405).json({ error: 'Method not allowed' })
   }
 }
